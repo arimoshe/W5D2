f2c1644f05bce9910303dbdf9f73d47dafa6ca98
Ari Moshe <arimoshe@gmail.com>
2022-07-12 15:19:00 -0700

    Finish select within select

--- skeleton/lib/03a_nobel_bonus.rb
+++ skeleton/lib/03a_nobel_bonus.rb
@@ -11,5 +11,12 @@ require_relative './sqlzoo.rb'
 def physics_no_chemistry
   # In which years was the Physics prize awarded, but no Chemistry prize?
   execute(<<-SQL)
+    SELECT 
+    yr
+    FROM
+    nobels WHERE
+    yr not in 
+    (select yr from nobels where subject = 'Chemistry')
+    and subject = 'physics'
   SQL
 end
--- skeleton/lib/04_select_within_select.rb
+++ skeleton/lib/04_select_within_select.rb
@@ -35,6 +35,12 @@ end
 def larger_than_russia
   # List each country name where the population is larger than 'Russia'.
   execute(<<-SQL)
+    SELECT 
+    name 
+    FROM
+    countries
+    WHERE
+    population > (select population from countries where name = 'Russia')
   SQL
 end
 
@@ -42,6 +48,13 @@ def richer_than_england
   # Show the countries in Europe with a per capita GDP greater than
   # 'United Kingdom'.
   execute(<<-SQL)
+    SELECT 
+    name 
+    FROM
+    countries
+    where
+    (gdp/population) > (select gdp/population from countries where name = 'United Kingdom')
+    and continent = 'Europe'
   SQL
 end
 
@@ -49,6 +62,12 @@ def neighbors_of_certain_b_countries
   # List the name and continent of countries in the continents containing
   # 'Belize', 'Belgium'.
   execute(<<-SQL)
+  SELECT
+  name, continent
+  FROM 
+  countries
+  WHERE
+  continent in (select continent from countries where name in ('Belize','Belgium'))
   SQL
 end
 
@@ -56,6 +75,13 @@ def population_constraint
   # Which country has a population that is more than Canada but less than
   # Poland? Show the name and the population.
   execute(<<-SQL)
+  SELECT
+  name, population
+  FROM
+  countries
+  where
+  population > (select population from countries where name = 'Canada')
+  and population < (select population from countries where name = 'Poland')
   SQL
 end
 
@@ -65,5 +91,13 @@ def sparse_continents
   # population.
   # Hint: Sometimes rewording the problem can help you see the solution.
   execute(<<-SQL)
+  SELECT
+  name, continent , population
+  from 
+  countries
+  WHERE
+  continent not in (
+    select continent from countries  where Population > 25000000
+  )
   SQL
 end
